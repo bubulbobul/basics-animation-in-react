3d67ee202c863651a239bf856ae56ece0b9ec3b2
bubulbobul <windam@live.fr>
2019-08-28 12:41:40 +0530

    Updates

---
+++ precache-manifest.3c7579ff5938a9b8bc0fe2d148d7c6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c17206e1c29397ada8f322dc3eab02f",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "c97c8f5d07ef75c5c1c7",
+    "url": "/repository-name/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6a38b00ec9fd6a9579ba",
+    "url": "/repository-name/static/js/2.90e2d4a2.chunk.js"
+  },
+  {
+    "revision": "c97c8f5d07ef75c5c1c7",
+    "url": "/repository-name/static/js/main.c022995c.chunk.js"
+  },
+  {
+    "revision": "7a7599673fdf02027830",
+    "url": "/repository-name/static/js/runtime~main.52b26a13.js"
+  }
+]);
